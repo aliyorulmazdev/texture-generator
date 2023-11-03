ebff2a0c512a2ed1e3eaec6cfb025f290d77f9b6
Kübra Yorulmaz <kbr.ercenk@gmail.com>
2023-11-03 15:37:01 +0300

    Google analytics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-  import React from 'react';
-  import Generator from './Generator';
-  import { ToastContainer } from "react-toastify";
-  import "react-toastify/dist/ReactToastify.css";
-  import './clarity';
+import React from 'react';
+import Generator from './Generator';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import './clarity';
+import ReactGA from 'react-ga';
 
-  function App() {
-    return (
-      <div className="App">
-        <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
-        <Generator />
-      </div>
-    );
-  }
+const TRACKING_ID = "UA-6345750229-G-6Y2T0WEP2C"; // Google Analytics ölçüm kimliği
+ReactGA.initialize(TRACKING_ID);
 
-  export default App;
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+      <Generator />
+    </div>
+  );
+}
+
+export default App;
